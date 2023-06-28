c025bf397bf34866c16f52c42997d2b64dbee423
Handan Yenial <88174651+HandanYenial@users.noreply.github.com>
2023-06-27 21:17:44 -0400

    Update LawsuitList.js

--- src/lawsuits/LawsuitList.js
+++ src/lawsuits/LawsuitList.js
@@ -14,12 +14,13 @@ function LawsuitList(){
 
     const [lawsuits , setLawsuits] = useState(null);
 
-    useEffect(function getAllLawsuitsOnMount(){//useEffect: is used for changing the state of the component
+    useEffect(function getAllLawsuitsOnMount(){       //useEffect: is used for changing the state of the component
         console.debug("LawsuitList useEffect getAllLawsuitsOnMount");
         search();
     }, []);
 
     //Triggered by search form submit; reloads lawsuits;
+    
     async function search(title){
         let lawsuits = await GlobalApi.getLawsuits(title);
         setLawsuits(lawsuits);
@@ -44,7 +45,6 @@ function LawsuitList(){
                 ? <LawsuitCardList lawsuits = { lawsuits }/>
                 : <p className = "lead"> No results were found</p>
             }
-        
         </div>
     </div>
     );
